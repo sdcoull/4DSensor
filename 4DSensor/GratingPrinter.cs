b6e1e53295992536680d703b19223bb86da53b34
Steven Coull <sdcoull@gmail.com>
2016-06-10 18:45:47 +0900

    Refactored Q1&2 to own class

---
+++ 4DSensor/GratingPrinter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+
+namespace DSensor
+{
+	public class GratingPrinter
+	{
+		public void OneDimensionalPrinter()
+		{
+			Console.WriteLine("Drawing 1D grating image...");
+
+			int width = 3800; //roughly 10cm in pixels, assuming the dpi is 96
+			int height = 3800;
+
+			using (Image output = new Bitmap(width, height))
+			{
+				Graphics graphics = Graphics.FromImage(output);
+				graphics.FillRectangle(Brushes.White, 0, 0, width, height);
+
+				int x = 0;
+				while (x < width)
+				{
+					graphics.FillRectangle(Brushes.Black, x, 0, MmToPixels(1, graphics.DpiX), height);
+					x += MmToPixels(2, graphics.DpiX);
+				}
+
+				output.Save("output - q1.bmp");
+			}
+
+			Console.WriteLine("1D grating image - done!");
+		}
+
+		public void TwoDimensionalPrinter()
+		{
+			Console.WriteLine("Drawing 2D grating image...");
+
+			int width = 3800;
+			int height = 3800;
+
+			using (Image output = new Bitmap(width, height))
+			{
+				Graphics graphics = Graphics.FromImage(output);
+				graphics.FillRectangle(Brushes.White, 0, 0, width, height);
+
+				int x = 0;
+				int y = 0;
+				//drawing the horizontal lines first to replicate example image, where vertical lines are on top
+				while (y < width)
+				{
+					graphics.FillRectangle(Brushes.Black, 0, y, width, MmToPixels(1, graphics.DpiX));
+					//draws a second grey rectangle at an offset of y + 0.1mm and height of 0.8mm
+					graphics.FillRectangle(Brushes.Gray, 0, y + MmToPixels(0.1, graphics.DpiY),
+										   width, MmToPixels(0.8, graphics.DpiY));
+					y += MmToPixels(2, graphics.DpiY);
+				}
+				while (x < height)
+				{
+					graphics.FillRectangle(Brushes.Black, x, 0, MmToPixels(1, graphics.DpiX), height);
+					graphics.FillRectangle(Brushes.Gray, x + MmToPixels(0.1, graphics.DpiX), MmToPixels(0.1, graphics.DpiX),
+										   MmToPixels(0.8, graphics.DpiX), height - MmToPixels(0.1, graphics.DpiX));
+					x += MmToPixels(2, graphics.DpiX);
+				}
+
+				output.Save("output - q2.bmp");
+			}
+			Console.WriteLine("2D grating image - done!");
+		}
+
+		private int MmToPixels(double mm, double dpi)
+		{
+			return (int)Math.Round(mm * dpi / 2.54d);
+		}
+	}
+}
+
